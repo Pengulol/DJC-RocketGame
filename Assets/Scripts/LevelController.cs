748cb1179afd0cf290b6bb6ab29cf3c6738c4564
Ap-Cristian <cristian.apetroaie@student.unitbv.ro>
2024-04-02 15:28:51 +0300

    first level, level init logic

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public GameObject _StartPoint;
+    [SerializeField]
+    public GameObject _EndPoint;
+    [SerializeField]
+    public GameObject _Player;
+    private void levelInit()
+    {
+        var playerInstance = Instantiate(_Player);
+        playerInstance.transform.position = _StartPoint.transform.position;
+        playerInstance.transform.rotation = _StartPoint.transform.rotation;
+    }
+    void Start()
+    {
+        this.levelInit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
